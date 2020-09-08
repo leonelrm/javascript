4525c9e254ee4ed1e089456b713aa0381fb48f8a
leonelrm <69789534+leonelrm@users.noreply.github.com>
2020-09-08 16:52:23 +0200

    update 11

---
+++ js_evenement/script/js_evt.js
@@ -0,0 +1,21 @@
+//alert("Bienvenue dans ma Page \n message stocke dans js_evt")
+let nom = prompt("quel est votre nom?");
+alert('bonjour ' + nom + ' comment allez vous aujourd\'hui ' )
+
+/*
+click
+dblclick
+mouseOver
+mouseDown
+mouseUp
+keyPress
+focus
+blur contraire a focus
+change: quand le contenu change
+select
+load
+unLoad
+dragstart
+dragover
+drop
+ */
